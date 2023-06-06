14fe3011c6410a63ca08834d177749f68f90868c
thefaraazkhan <khanmohdfaraaz@gmail.com>
2023-06-06 21:30:36 +0530

    added user following/unfollowing feature

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("./controllers/userController");
 const postController = require("./controllers/postController");
+const followController = require("./controllers/followController");
 
 router.get("/", userController.home);
 
@@ -9,12 +10,29 @@ router.get("/", userController.home);
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
+router.post("/doesUsernameExist", userController.doesUsernameExist);
+router.post("/doesEmailExist", userController.doesEmailExist);
 
 // Profile related routes
 router.get(
   "/profile/:username",
   userController.ifUserExists,
+  userController.sharedProfileData,
   userController.profilePostsScreen
+);
+
+router.get(
+  "/profile/:username/followers",
+  userController.ifUserExists,
+  userController.sharedProfileData,
+  userController.profileFollowersScreen
+);
+
+router.get(
+  "/profile/:username/following",
+  userController.ifUserExists,
+  userController.sharedProfileData,
+  userController.profileFollowingScreen
 );
 
 // Posts related routes
@@ -43,6 +61,20 @@ router.post(
   "/post/:id/delete",
   userController.mustBeLoggedIn,
   postController.delete
+);
+router.post("/search", postController.search);
+
+// Follow related routes
+router.post(
+  "/addFollow/:username",
+  userController.mustBeLoggedIn,
+  followController.addFollow
+);
+
+router.post(
+  "/removeFollow/:username",
+  userController.mustBeLoggedIn,
+  followController.removeFollow
 );
 
 module.exports = router;
